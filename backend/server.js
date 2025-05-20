9de62da4a84019440522e4628cfc8096a15542ad
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-05-20 17:54:45 +0500

    crud

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+// imports
+// const express = require("express");
+import express from 'express';
+import dotenv from 'dotenv';
+import connectToDb from './config/connectToDb.js';
+import productRouter from './routers/productRouter.js';
+import cors from 'cors';
+
+dotenv.config()
+
+
+// create express app
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+connectToDb();
+
+app.use('/products', productRouter)
+
+// start app / server
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running at http://localhost:${process.env.PORT}`);
+})
+
+
+/*
+
+npm init --y
+npm install express
+npm run dev
+npm install dotenv
+
+*/
---
+++ frontend/admin panel/src/pages/products.jsx
@@ -0,0 +1,100 @@
+import { useCallback, useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Box, Button, Card, Container, Divider, Stack, Typography } from '@mui/material';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import axios from 'axios';
+
+const Products = () => {
+
+    const [products, setProducts] = useState([]);
+    
+    let fetchData = async () => {
+        let response = await axios.get("http://localhost:3001/products");
+        console.log(response);
+        setProducts(response.data.products)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    return (
+        <>
+            <Helmet>
+                <title>
+                    Orders | Carpatin Free
+                </title>
+            </Helmet>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    py: 8
+                }}
+            >
+                <Container maxWidth="xl">
+                    <Stack spacing={3}>
+                        <Stack
+                            alignItems="flex-start"
+                            direction="row"
+                            justifyContent="space-between"
+                            spacing={3}
+                        >
+                            <Typography variant="h4">
+                                Products
+                            </Typography>
+                            <Button
+                                color="primary"
+                                size="large"
+                                variant="contained"
+                            >
+                                Add
+                            </Button>
+                        </Stack>
+                        <div>
+                            <Card>
+                                {/* TABLE STARTS HERE */}
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="center">Product Name</TableCell>
+                                                <TableCell align="center">Product Price</TableCell>
+                                                <TableCell align="center">Product Description</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {products.map((product) => (
+                                                <TableRow
+                                                    key={product._id}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">
+                                                        {product.pname}
+                                                    </TableCell>
+                                                    <TableCell align="center">{product.pprice}</TableCell>
+                                                    <TableCell align="left">{product.pdesc}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                                {/* TABLE ENDS HERE */}
+
+                            </Card>
+                        </div>
+                    </Stack>
+                </Container>
+            </Box>
+        </>
+    );
+};
+
+export default Products;
