1bafefe229e3be88f116d546cf89358e98ae7b59
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-05-27 15:46:22 +0500

    cat crud

--- frontend/admin panel/src/pages/categories.jsx
+++ frontend/admin panel/src/pages/categories.jsx
@@ -3,17 +3,39 @@ import { Helmet } from 'react-helmet-async';
 import {
     Box, Button, Card, Container, Divider, Stack, Typography,
     TextField,
-    Unstable_Grid2 as Grid
+    Unstable_Grid2 as Grid, IconButton, Alert
 } from '@mui/material';
 import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 import axios from 'axios';
 
 const Categories = () => {
-
     const [category, setCategory] = useState({
         categoryname: ""
     })
+    const [categories, setCategories] = useState([]);
+    const [alert, setAlert] = useState({
+        message: "",
+        type: ""
+    })
+    let fetchData = async () => {
+        let response = await axios.get("http://localhost:3001/categories");
+        console.log(response);
+        setCategories(response.data.categories)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     const handleCategoryChange = (e) => {
         let { name, value } = e.target;
@@ -25,20 +47,41 @@ const Categories = () => {
             }
         })
     }
-
     const handleCategorySubmit = async () => {
+        try {
+            let response = await axios.post("http://localhost:3001/categories", category)
+            // console.log(response)
+            setCategory({
+                categoryname: ""
+            })
+            setAlert({
+                message: "Category Added",
+                type: "success"
+            })
+            fetchData()
 
-        let response = await axios.post("http://localhost:3001/categories", category)
+        }
+        catch (err) {
+            setAlert({
+                message: err.message,
+                type: "error"
+            })
+        }
 
-        console.log(response)
 
+    }
+
+    const handleCategoryDelete = async (id) => {
+        let response = await axios.delete(`http://localhost:3001/categories/${id}`)
+
+        fetchData()
     }
 
     return (
         <>
             <Helmet>
                 <title>
-                    Orders | Carpatin Free
+                    Ecomm - Admin
                 </title>
             </Helmet>
             <Box
@@ -62,6 +105,9 @@ const Categories = () => {
 
                         </Stack>
                         <div>
+                            <Alert severity={alert.type}>{alert.message}</Alert>
+
+                            {/* Category Form */}
                             <Card sx={{ p: 3 }}>
 
                                 <Stack spacing={3}>
@@ -84,6 +130,40 @@ const Categories = () => {
                                         </Button>
                                     </Box>
                                 </Stack>
+
+                            </Card>
+                            {/* Categories Table */}
+
+                            <Card sx={{ p: 3 }}>
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="left">Category Name</TableCell>
+                                                <TableCell align="center"></TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {categories.map((item) => (
+                                                <TableRow
+                                                    key={item._id}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">
+                                                        {item.categoryname}
+                                                    </TableCell>
+                                                    <TableCell align="right">
+                                                        <IconButton aria-label="delete" color='error' onClick={() => handleCategoryDelete(item._id)}>
+                                                            <DeleteIcon />
+                                                        </IconButton>
+                                                        <IconButton aria-label="delete" color='success'>
+                                                            <EditIcon />
+                                                        </IconButton>                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
                             </Card>
                         </div>
                     </Stack>
